f5dc4c5fb24604a8ef6bc255752473cc2969bba1
Carlos <solracjunio@gmail.com>
2024-08-20 18:28:29 -0300

    make windown

--- src/triangle.nim
+++ src/triangle.nim
@@ -1,7 +1,28 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
+#import triangle/submodule
 
-import triangle/submodule
+#when isMainModule:
+#  echo(getWelcomeMessage())
 
-when isMainModule:
-  echo(getWelcomeMessage())
+import raylib
+
+const
+  screenWidth: int32 = 800
+  screenHeight: int32 = 800
+  screenName: string = "Cube3D"
+  fps: int32 = 60
+
+proc main = 
+  initWindow(screenWidth, screenHeight, screenName)
+  setTargetFPS(fps)
+
+  while not windowShouldClose():
+
+    beginDrawing()
+    clearBackground(Black)
+    endDrawing()
+
+  closeWindow()
+
+main()
